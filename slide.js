ad4e36c594ad74d59be2a5dd578480d96049d0dd
wanwan964 <pochi172107@gmail.com>
2024-09-19 08:57:44 +0900

    Add files via upload

---
+++ slide.js
@@ -0,0 +1,34 @@
+'use strict';
+{
+    const images=['ro2.jpg','ro1.jpg',]
+    const slideImg = document.getElementById('slide_img');
+    const prev = document.getElementById('prev');
+    const page = document.getElementById('pabe');
+    const next = document.getElementById('next');
+    let current = 0;
+   
+    
+
+
+    next.addEventListener('click',function(){
+        
+            current=1;
+        
+
+
+            slideImg.src = images[current];
+
+    });
+    prev.addEventListener('click',function(){
+        
+        current=0;
+    
+
+
+        slideImg.src = images[current];
+
+    });
+}
+
+    
+      
