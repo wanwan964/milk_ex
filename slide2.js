75e14b39fa3f977e031033253fbfb5f8de16ecd2
wanwan964 <pochi172107@gmail.com>
2024-09-23 14:26:56 +0900

    Add files via upload

---
+++ slide2.js
@@ -0,0 +1,35 @@
+'use strict';
+{
+    const images=['kojin.jpg','gu1.jpg',]
+    const slideImg = document.getElementById('slide_img');
+    const prev = document.getElementById('prev');
+    const page = document.getElementById('pabe');
+    const next = document.getElementById('next');
+    let current = 0;
+   
+    
+
+    
+    next.addEventListener('click',function(){
+        
+            current=1;
+            
+        
+
+
+            slideImg.src = images[current];
+
+    });
+    prev.addEventListener('click',function(){
+        
+        current=0;
+    
+
+
+        slideImg.src = images[current];
+
+    });
+}
+
+    
+      
